69f9906ac41fd8fd1766bb0e72f8329ec383eb35
varodrig <varodrig@redhat.com>
2021-07-23 15:46:40 -0400

    UI implementation + config url

---
+++ frontend/logic/run_template.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#https://pip.pypa.io/en/stable/installing/
+pip install j2
+pip install j2cli
+wget https://files.pythonhosted.org/packages/97/11/20f564c5d57024500e7bc3bc011d8c456f6041611683327c5e571d866efe/j2-1.2.1.tar.gz
